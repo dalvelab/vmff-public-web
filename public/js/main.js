7f5a426a8904fc2cb44070646274edc31a8b2ba7
Vlad Bragin <dalvelab@gmail.com>
2020-06-13 01:17:28 +0500

    fix sidebar animation

--- public/js/main.js
+++ public/js/main.js
@@ -7,21 +7,18 @@ const links = document.querySelectorAll(".sidebar .menu .link");
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
-    sidebar.style.marginLeft = "-300px";
-    sidebar.style.opacity = "0";
+    sidebar.style.left = "-300px";
     overlay.style.display = "none";
   });
 });
 
 const openSidebar = () => {
-  sidebar.style.marginLeft = "0px";
-  sidebar.style.opacity = "1";
+  sidebar.style.left = "0";
   overlay.style.display = "block";
 };
 
 const closeSidebar = () => {
-  sidebar.style.marginLeft = "-300px";
-  sidebar.style.opacity = "0";
+  sidebar.style.left = "-300px";
   overlay.style.display = "none";
 };
 
@@ -29,6 +26,6 @@ openSidebarBtn.addEventListener("click", openSidebar);
 closeSidebarbtn.addEventListener("click", closeSidebar);
 
 overlay.addEventListener("click", () => {
-  sidebar.style.marginLeft = "-300px";
+  sidebar.style.left = "-300px";
   overlay.style.display = "none";
 });
