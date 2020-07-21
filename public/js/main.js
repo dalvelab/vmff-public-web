8caf8de4c1bb677deae8c38cc9028c64e534141c
Vlad Bragin <dalvelab@gmail.com>
2020-07-21 16:53:40 +0500

    fix program page & add close modal functionality

--- public/js/main.js
+++ public/js/main.js
@@ -30,15 +30,23 @@ closeSidebarbtn.addEventListener("click", closeSidebar);
 overlay.addEventListener("click", () => {
   sidebar.style.left = "-300px";
   overlay.style.display = "none";
+  modal.style.display = "none";
 });
 
 // MAIL SUBSCRIPTION
 const modal = document.querySelector(".modal");
 const emailSubscribe = document.querySelector(".send-email-btn");
+const closeModal = document
+  .querySelector("#close_modal")
+  .addEventListener("click", () => {
+    modal.style.display = "none";
+    overlay.style.display = "none";
+  });
 
 emailSubscribe.addEventListener("click", showModal);
 
-function showModal() {
+function showModal(e) {
+  e.preventDefault();
   modal.style.display = "grid";
   overlay.style.display = "block";
 }
