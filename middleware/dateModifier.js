0aa1d862b03df8686bd007ec6d831011b711d5e6
Vlad Bragin <dalvelab@gmail.com>
2020-08-02 14:18:07 +0500

    fix passed-day-blur algorithm_3

--- middleware/dateModifier.js
+++ middleware/dateModifier.js
@@ -2,9 +2,9 @@ function modifyDate(date) {
   let modifiedDate = date.split(".");
   modifiedDate = modifiedDate.map((date) => {
     if (date.charAt(0) === "0") {
-      return date.substr(1, 1);
+      return Number(date.substr(1, 1));
     } else {
-      return date.substr(0, 2);
+      return Number(date.substr(0, 2));
     }
   });
   return modifiedDate;
